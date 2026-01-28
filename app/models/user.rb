92f3f263827ec2dd03e33836be42f988eb7e3bc5
takamine <takamine.yuta@gmail.com>
2026-01-28 16:47:38 +0900

    ゲストユーザー生成ロジックを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recipes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_recipes, through: :favorites, source: :recipe
+
+  def self.guest
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64(16)
+    end
+  end
 end
