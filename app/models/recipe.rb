5517a0725400b7f5b455e842693101fc4f6e32bd
takamine <takamine.yuta@gmail.com>
2026-01-28 21:56:44 +0900

    レシピCRUDの画面（一覧/詳細/投稿/編集/削除）を追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,4 +8,12 @@ class Recipe < ApplicationRecord
   validates :memo, length: { maximum: 5000 }, allow_blank: true
 
   validates :source_url, format: { with: /\Ahttps?:\/\/.+\z/i, message: "はhttp(s)から始まるURLを入力してください" }, allow_blank: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[created_at id ingredients memo source_url title updated_at user_id]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[user favorites]
+  end
 end
