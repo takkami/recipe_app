ee0c92594fe451002da6aebd8a9538a17e5246ed
takamine <takamine.yuta@gmail.com>
2026-01-28 17:08:04 +0900

    ゲストログイン導線を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root "home#index"
+
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in", as: :users_guest_sign_in
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
